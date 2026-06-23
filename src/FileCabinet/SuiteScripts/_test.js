f9b7e5a305cb4bb7ee51f9f515ba7d67f65660e0
Even Yohans <evanjephi@gmail.com>
2026-06-23 11:18:32 -0400

    moved file

--- src/FileCabinet/SuiteScripts/_test.js
+++
@@ -1,9 +0,0 @@
-const comm = 10 
-const totalAmt = 150
-
-const commission = (comm / 100) * totalAmt
-
-const overage = 20
-const totalCom = commission + overage
-console.log(commission)
-console.log(totalCom)
